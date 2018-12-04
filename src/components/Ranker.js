a40d6132fcdf15033cb8930c91610e37bf8f238d
Anesu Mafuvadze <mafuvadzeanesu@gmail.com>
2018-12-04 03:29:42 -0500

    final

--- src/components/Ranker.js
+++ src/components/Ranker.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import Question from './Question'
+import Question from './Question';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import Container from 'react-bootstrap/lib/Container';
 import { Header, Button, Label } from 'semantic-ui-react'
 import NavBar from '../components/Navbar';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
@@ -18,6 +20,8 @@ class Ranker extends Component {
             answerChecked: false,
             currentVersus: null,
             correct: null,
+            showResults: false,
+            numCorrect: 0,
         }
     }
 
@@ -42,15 +46,19 @@ shuffle = (a) => {
     handleCheckAnswer = (topicSet) => {
         const notChosen = this.state.selectedFood === this.state.currentVersus[0]
             ? this.state.currentVersus[1] : this.state.currentVersus[0];
+        console.log(topicSet.rank.indexOf(this.state.selectedFood), topicSet.rank.indexOf(notChosen));    
         if (topicSet.rank.indexOf(this.state.selectedFood) < topicSet.rank.indexOf(notChosen)) {
             this.setState({
                 correct: this.state.selectedFood,
                 answerChecked: true,
+                done: this.questionNum == 4,
+                numCorrect: this.state.numCorrect + 1,
             });
         } else {
             this.setState({
                 correct: notChosen,
                 answerChecked: true,
+                done: this.state.questionNum === 4,
             });
         }
     }
@@ -70,7 +78,15 @@ shuffle = (a) => {
         return (
             <div>
                 <NavBar/>
-                <div style={{ backgroundColor: '#0080ff' }}>
+                {this.state.showResults && <div>
+                    <Jumbotron fluid style={{ backgroundColor: 'white', marginBottom: "0" }}>
+                        <Container>
+                            {<div style={{textAlign:'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}><h1 style={{fontSize: '3em'}}>{`You answered ${this.state.numCorrect} out of ${5} correct`}</h1><Button style={{width: '30%'}} content='Done'><Link to='/home'>Done</Link></Button></div>}
+                        </Container>
+                    </Jumbotron>
+                </div>}
+                
+                {!this.state.showResults && <div style={{ backgroundColor: '#0080ff' }}>
                     <Header size='huge' style={{ display: 'flex', justifyContent: 'center', padding: "36px", color: 'white' }}>
                         {title}
                     </Header>
@@ -92,7 +108,7 @@ shuffle = (a) => {
 
                         <Question
                             correct={this.state.correct == null ? null : this.state.correct === foods.foods[randVersus[1]].name}
-                            selected={foods.foods[randVersus[1]].name === this.state.selectedFood}x
+                            selected={foods.foods[randVersus[1]].name === this.state.selectedFood} x
                             onClick={() => {
                                 this.handleCardClick(foods.foods[randVersus[1]].name);
                             }}
@@ -102,24 +118,24 @@ shuffle = (a) => {
                             image={foods.foods[randVersus[1]].image}
                         />
                     </div>
-                    <div> {this.state.correct != null && <Header style={{ display: 'flex', justifyContent: 'center', margin: "36px", color: 'white'}} >{this.state.correct} is the answer</Header>}</div>
+                    <div> {this.state.correct != null && <Header style={{ display: 'flex', justifyContent: 'center', margin: "36px", color: 'white' }} >{this.state.correct} is the answer</Header>}</div>
                     <div style={{ display: 'flex', justifyContent: 'center', padding: '20px 0' }}>
-                        {this.state.answerChosen && <Button onClick={() => { this.handleCheckAnswer(topicSet)}} positive>Check Answer</Button>}
+                        {this.state.answerChosen && <Button onClick={() => { this.handleCheckAnswer(topicSet) }} positive>Check Answer</Button>}
                         {this.state.done && <Button content='Done'><Link to='/home'>Done</Link></Button>}
-                        {(!this.state.done && this.state.answerChecked) && <Button
-                            content='Next'
+                        {(!this.state.showResults && this.state.answerChecked) && <Button
+                            content={this.state.questionNum === 4 ? 'Results' : 'Next'}
                             icon='right arrow'
                             labelPosition='right'
                             onClick={() => {
                                 const qnum = this.state.questionNum + 1;
                                 if (qnum === 4) {
-                                    this.setState({ done: true, questionNum: qnum, answerChosen: false, answerChecked: false, correct: null, selected: null, currentVersus: null,})
+                                    this.setState({questionNum: qnum, answerChosen: false, answerChecked: false, correct: null, selected: null, currentVersus: null, showResults: true})
                                 } else {
                                     this.setState({ questionNum: qnum, answerChosen: false, answerChecked: false, correct: null, currentVersus: null, selected: null });
                                 }
                             }} />}
                     </div>
-                </div>
+                </div>}
             </div>
         );
     }
--- src/resources/food.js
+++ src/resources/food.js
@@ -13,19 +13,19 @@ export default {
             restaurant: "",
             ingredients: ['noodles', 'pork', 'eggs', 'onions', 'soup'],
         },
-        "Panda": {
+        "Friend Rice with Chow Mein": {
             image: "https://s3-media4.fl.yelpcdn.com/bphoto/S8UVZoP9cyzLqRNBUrovOQ/348s.jpg",
             name: "Friend Rice with Chow Mein",
             restaurant: "Panda Expresss",
             ingredients: ['rice', 'chow mein', 'orange chicken'],
         },
-        "Spaghetti": {
+        "Meatball and Spaghetti": {
             image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/12/0/FN_Ina-Garten-Real-Meatballs-and-Spaghetti_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384540886901.jpeg",
             name: "Meatball and Spaghetti",
             restaurant: "Il Forno",
             ingredients: ['spaghetti', 'tomato sauce', 'meatballs', 'parmesan cheese'],
         },
-        "Banana pancakes": {
+        "Banana Pancakes": {
             image: "https://images.huffingtonpost.com/2016-04-07-1460049841-1325332-BananaPancakes-thumb.jpg",
             name: "Banana Pancakes",
             restaurant: "Pitch",
@@ -59,23 +59,23 @@ export default {
     "questions": {
         balance: {
             title: "Which meal is more balanced?",
-            rank: ["Turkey Avocado Sandwich", "Grilled Peanut Butter Jelly with Banana", "Fried Chicken", "Ramen", "Blue Plate", "Spaghetti", "Panda", "Chicken Tenders", "Banana pancakes", ],
-            versus: [["Turkey Avocado Sandwich", "Blue Plate"], ["Grilled Peanut Butter Jelly with Banana", "Panda"], ["Fried Chicken", "Panda"], ["Chicken Tenders", "Turkey Avocado Sandwich"], ["Fried Chicken", "Banana pancakes"]]
+            rank: ["Turkey Avocado Sandwich", "Grilled Peanut Butter Jelly with Banana", "Fried Chicken", "Ramen", "Blue Plate", "Meatball and Spaghetti", "Friend Rice with Chow Mein", "Chicken Tenders", "Banana Pancakes", ],
+            versus: [["Turkey Avocado Sandwich", "Blue Plate"], ["Grilled Peanut Butter Jelly with Banana", "Friend Rice with Chow Mein"], ["Fried Chicken", "Friend Rice with Chow Mein"], ["Chicken Tenders", "Turkey Avocado Sandwich"], ["Fried Chicken", "Banana Pancakes"]]
         },
         variety: {
             title: "Which meal is more varied?",
-            rank: ["Turkey Avocado Sandwich", "Fried Chicken", "Grilled Peanut Butter Jelly with Banana", "Blue Plate", "Ramen", "Panda", "Spaghetti", "Chicken Tenders", "Banana pancakes"],
-            versus: [["Turkey Avocado Sandwich", "Panda"], ["Fried Chicken", "Banana pancakes"], ["Grilled Peanut Butter Jelly with Banana", "Chicken Tenders"], ["Ramen", "Spaghetti"], ["Panda", "Banana pancakes"]]
+            rank: ["Turkey Avocado Sandwich", "Fried Chicken", "Grilled Peanut Butter Jelly with Banana", "Blue Plate", "Ramen", "Friend Rice with Chow Mein", "Meatball and Spaghetti", "Chicken Tenders", "Banana Pancakes"],
+            versus: [["Turkey Avocado Sandwich", "Friend Rice with Chow Mein"], ["Fried Chicken", "Banana Pancakes"], ["Grilled Peanut Butter Jelly with Banana", "Chicken Tenders"], ["Ramen", "Meatball and Spaghetti"], ["Friend Rice with Chow Mein", "Banana Pancakes"]]
         },
         adequate: {
             title: "Which meal is more adequate?",
-            rank: ["Fried Chicken", "Blue Plate", "Turkey Avocado Sandwich", "Grilled Peanut Butter Jelly with Banana", "Blue Plate", "Spaghetti", "Panda", "Ramen", "Banana pancakes", "Chicken Tenders"],
-            versus: [["Fried Chicken", "Banana pancakes"], ["Blue Plate", "Ramen"], ["Turkey Avocado Sandwich", "Chicken Tenders"], ["Grilled Peanut Butter Jelly with Banana", "Panda"], ["Chicken Tenders", "Spaghetti"]]
+            rank: ["Fried Chicken", "Blue Plate", "Turkey Avocado Sandwich", "Grilled Peanut Butter Jelly with Banana", "Blue Plate", "Meatball and Spaghetti", "Friend Rice with Chow Mein", "Ramen", "Banana Pancakes", "Chicken Tenders"],
+            versus: [["Fried Chicken", "Banana Pancakes"], ["Blue Plate", "Ramen"], ["Turkey Avocado Sandwich", "Chicken Tenders"], ["Grilled Peanut Butter Jelly with Banana", "Friend Rice with Chow Mein"], ["Chicken Tenders", "Meatball and Spaghetti"]]
         },
         moderate: {
             title: "Which meal is more moderate?",
-            rank: ["Blue Plate", "Turkey Avocado Sandwich", "Spaghetti", "Banana pancakes", "Grilled Peanut Butter Jelly with Banana", "Panda", "Chicken Tenders", "Fried Chicken", "Ramen"],
-            versus: [["Blue Plate", "Chicken Tenders"], ["Fried Chicken", "Turkey Avocado Sandwich"], ["Ramen", "Spaghetti"], ["Grilled Peanut Butter Jelly with Banana", "Fried Chicken"], ["Banana pancakes", "Panda"]]
+            rank: ["Blue Plate", "Turkey Avocado Sandwich", "Meatball and Spaghetti", "Banana Pancakes", "Grilled Peanut Butter Jelly with Banana", "Friend Rice with Chow Mein", "Chicken Tenders", "Fried Chicken", "Ramen"],
+            versus: [["Blue Plate", "Chicken Tenders"], ["Fried Chicken", "Turkey Avocado Sandwich"], ["Ramen", "Meatball and Spaghetti"], ["Grilled Peanut Butter Jelly with Banana", "Fried Chicken"], ["Banana Pancakes", "Friend Rice with Chow Mein"]]
         }
     }
 }
